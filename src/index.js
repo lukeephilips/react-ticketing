6f2da25cb0d7420a4ea4452f4aa7aa5ebf102985
Courtney Phillips <courtney@epicodus.com>
2017-06-12 12:03:49 -0700

    Hard-code sample help queue ticket elements, including names, location, and wait time.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 var heading = React.createElement('h1', {}, 'Help Queue');
+var ticketLocation = React.createElement('h3', {}, '3a');
+var ticketNames = React.createElement('h3', {}, 'Thato and Haley');
+var ticketWaitTime = React.createElement('h3', {}, '4 mins');
 var clock = React.createElement('h2', {}, `${new Date().toLocaleTimeString()}`);
 var app = React.createElement('div', {}, heading, clock);
 
