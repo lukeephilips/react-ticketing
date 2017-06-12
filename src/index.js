aa93b56653644e690f52d0d68ac8017c52cb2d5d
Courtney Phillips <courtney@epicodus.com>
2017-06-12 12:05:46 -0700

    Replace all code in index.js with its JSX-syntax-equivalent.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-var heading = React.createElement('h1', {}, 'Help Queue');
-var ticketLocation = React.createElement('h3', {}, '3a');
-var ticketNames = React.createElement('h3', {}, 'Thato and Haley');
-var ticketWaitTime = React.createElement('h3', {}, '4 mins');
-var clock = React.createElement('h2', {}, `${new Date().toLocaleTimeString()}`);
-var app = React.createElement('div', {}, heading, clock, ticketLocation, ticketNames, ticketWaitTime);
-
 ReactDOM.render(
-  app,
+  <div>
+    <h1>Help Queue</h1>
+    <h2>{new Date().toLocaleTimeString()}</h2>
+    <h3>3a</h3>
+    <h3>Thato and Haley</h3>
+    <h3>4 mins</h3>
+  </div>,
   document.getElementById('react-app-root')
 );
