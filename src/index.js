7e0d1a9ac0e927b01ba1ff80c79c35f30f50017e
Courtney Phillips <courtney@epicodus.com>
2017-06-12 12:04:23 -0700

    Add hard-coded ticket elements to parent element to render in browser.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var ticketLocation = React.createElement('h3', {}, '3a');
 var ticketNames = React.createElement('h3', {}, 'Thato and Haley');
 var ticketWaitTime = React.createElement('h3', {}, '4 mins');
 var clock = React.createElement('h2', {}, `${new Date().toLocaleTimeString()}`);
-var app = React.createElement('div', {}, heading, clock);
+var app = React.createElement('div', {}, heading, clock, ticketLocation, ticketNames, ticketWaitTime);
 
 ReactDOM.render(
   app,
