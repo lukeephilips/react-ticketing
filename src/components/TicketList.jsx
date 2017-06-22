c9245e67f5af6d70c5cd57bd5b941a6a39556207
tyler suderman <tcsuder@gmail.com>
2017-06-22 14:47:47 -0700

    add TicketList component

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TicketList(props){
+  return (
+    <div>
+      <h3>3A - Thato & Haley</h3>
+      <p><em>Firebase won't save record</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+export default TicketList;
