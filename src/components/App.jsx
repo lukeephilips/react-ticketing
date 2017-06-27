659f0f32b8311da3ad9ff770e3dd1add0961340b
tyler suderman <tcsuder@gmail.com>
2017-06-27 11:05:32 -0700

    refactor header into it's own component to match curriculum refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import TicketList from './TicketList';
+import Header from './Header';
 
 function App(props){
   return (
     <div>
-      <h1>Help Queue</h1>
+      <Header/>
       <TicketList/>
     </div>
   );
