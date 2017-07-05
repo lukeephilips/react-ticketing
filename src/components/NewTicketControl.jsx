1d96b8c0cba892f012ee40187ea86e8813ed60bd
tyler suderman <tcsuder@gmail.com>
2017-07-05 13:53:37 -0700

    add basic structure of NewTicketControl component

---
+++ src/components/NewTicketControl.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class NewTicketControl extends React.Component {
+
+  render(){
+    return (
+      <button>Request Help</button>
+    );
+  }
+
+}
+
+export default NewTicketControl;
