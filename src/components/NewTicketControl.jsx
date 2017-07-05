1728639b6ca0137d91bb78c4d7393b34bcbca88b
tyler suderman <tcsuder@gmail.com>
2017-07-05 14:36:10 -0700

    declare state in NewTicketControl component

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -2,11 +2,18 @@ import React from "react";
 
 class NewTicketControl extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {formVisibleOnPage: false};
+  }
+
+
   render(){
     return (
-      <button>Request Help</button>
+      <button onClick={this.handleDisplayingNewTicketForm}>Request Help</button>
     );
   }
+
 
 }
 
