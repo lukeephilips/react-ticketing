ec2afe2309751ac3df966e97afe6d2ad2feadb26
tyler suderman <tcsuder@gmail.com>
2017-07-06 17:35:36 -0700

    create handler method that retrieves references to inputs in dom

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 class NewTicketForm extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.handleNewTicketFormSubmission = this.handleNewTicketFormSubmission.bind(this);
+  }
+
   handleNewTicketFormSubmission(event) {
     event.preventDefault()
+    const { _names, _location, _issue } = this.refs;
+    alert(`Data Gathered! ${_names.value}, ${_location.value}, ${_issue.value}`);
   }
 
   render(){
@@ -11,18 +18,23 @@ class NewTicketForm extends React.Component {
       <div>
         <form onSubmit={this.handleNewTicketFormSubmission}>
           <input
+            ref="_names"
             type="text"
             id="names"
             placeholder="Pair Names"/>
           <input
+            ref="_location"
             type="text"
             id="location"
             placeholder="Location"/>
           <textarea
+            ref="_issue"
+            type="text"
             id="issue"
             placeholder="Describe your issue."/>
           <button type="submit">Help!</button>
         </form>
+
       </div>
     );
   }
