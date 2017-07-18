8dd7281b9c91458da4a7d43aafb7ce3ae453fbb9
tyler suderman <tcsuder@gmail.com>
2017-07-17 17:07:14 -0700

    add Ticket model

---
+++ src/models/Ticket.js
@@ -0,0 +1,9 @@
+class Ticket {
+  constructor(names, location, description) {
+    this.names = names;
+    this.location = location;
+    this.description = description;
+  }
+}
+
+export default Ticket;
