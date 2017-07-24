ecacdd8a2bc81ba78bc28b137b784203ed2f458a
tyler suderman <tcsuder@gmail.com>
2017-07-24 16:50:47 -0700

    refactor back to displaying initial value with Moment and rerender function available to Queue

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -10,6 +10,7 @@ function TicketList(props){
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
+          timeSinceOpened={ticket.timeSinceOpened}
           key={index}/>
       )}
     </div>
--- src/models/Ticket.js
+++ src/models/Ticket.js
@@ -1,8 +1,19 @@
+import Moment from "moment";
+
 class Ticket {
   constructor(names, location, description) {
     this.names = names;
     this.location = location;
     this.description = description;
+    this.timeOpened = new Date();
+    this.timeSinceOpened = this.setTimeSinceOpened();
+    console.log(this);
+  }
+  setTimeSinceOpened() {
+    let now = new Moment(new Date());
+    let then = new Moment(this.timeOpened);
+    let newTimeSinceOpened = then.from(now, true);
+    return newTimeSinceOpened;
   }
 }
 
