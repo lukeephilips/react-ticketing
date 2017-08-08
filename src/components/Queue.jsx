cf0c7127a9a2608a49bd578dfa0d844b094c414e
tyler suderman <tcsuder@gmail.com>
2017-08-08 09:35:28 -0700

    add console logs and additional lifecycle methods to watch process in console

--- src/components/Queue.jsx
+++ src/components/Queue.jsx
@@ -5,20 +5,70 @@ import NewTicketControl from './NewTicketControl';
 class Queue extends React.Component {
 
   constructor(props) {
+    console.log('constructor');
     super(props);
     this.state = {
       masterTicketList: [],
     };
     this.addNewTicketToList = this.addNewTicketToList.bind(this);
+    this.updateTicketTimeSinceOpened = this.updateTicketTimeSinceOpened.bind(this);
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    this.timeSinceOpenedChecker = setInterval(() =>
+      this.updateTicketTimeSinceOpened(),
+      5000
+    );
+  }
+
+  componentWillReceiveProps() {
+    console.log('componentWillReceiveProps');
+    console.log(this.props);
+  }
+
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate');
+    return true;
+  }
+
+  componentWillUpdate() {
+    console.log('componentWillUpdate');
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timeSinceOpenedChecker);
   }
 
   addNewTicketToList(newTicket){
-    var newMasterTicketList = this.state.masterTicketList.slice();
+    let newMasterTicketList = this.state.masterTicketList.slice();
     newMasterTicketList.push(newTicket);
+    console.log('setState');
+    console.log(newMasterTicketList);
     this.setState({masterTicketList: newMasterTicketList});
   }
 
+  updateTicketTimeSinceOpened() {
+    let newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.forEach((ticket) =>
+      ticket.setTimeSinceOpened()
+    );
+    console.log('setState');
+    console.log(newMasterTicketList);
+    this.setState({masterTicketList:newMasterTicketList})
+  }
+
   render() {
+    console.log('render');
     return (
       <div>
         <TicketList ticketList = {this.state.masterTicketList}/>
--- src/models/Ticket.js
+++ src/models/Ticket.js
@@ -1,19 +1,17 @@
 import Moment from "moment";
 
 class Ticket {
+
   constructor(names, location, description) {
     this.names = names;
     this.location = location;
     this.description = description;
-    this.timeOpened = new Date();
-    this.timeSinceOpened = this.setTimeSinceOpened();
-    console.log(this);
+    this.timeOpened = new Moment();
+    this.timeSinceOpened = 'a few seconds';
   }
+
   setTimeSinceOpened() {
-    let now = new Moment(new Date());
-    let then = new Moment(this.timeOpened);
-    let newTimeSinceOpened = then.from(now, true);
-    return newTimeSinceOpened;
+    this.timeSinceOpened = this.timeOpened.fromNow(true);
   }
 }
 
