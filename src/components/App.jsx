1e09204e36f28c4537084e4dfd79f6db61d69b69
tcsuder <tcsuder@gmail.com>
2017-08-11 12:32:41 -0700

    set up initial styles with css import in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from './Header';
 import Queue from './Queue';
+import styles from './styles/App.css';
 
 function App(props){
   return (
-    <div>
+    <div className = {styles.app}>
       <Header/>
       <Queue/>
     </div>
