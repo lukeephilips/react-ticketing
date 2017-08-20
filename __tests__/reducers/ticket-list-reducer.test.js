3b5502ceaf3da018dcbe7173ad2093bb53410450
tcsuder@gmail.com <tcsuder@gmail.com>
2017-08-20 11:07:35 -0700

    import files and test with failing expect calls

--- __tests__/reducers/ticket-list-reducer.test.js
+++ __tests__/reducers/ticket-list-reducer.test.js
@@ -1,8 +1,14 @@
+import ticketList from './../../src/reducers/ticket-list-reducer';
+import c from './../../src/constants';
+
 describe("Ticket list reducer", () => {
 
-  test('temporary dummy test: two plus two is four', () => {
-    expect(2 + 1).toBe(3);
+  test('should return equivilant state if no action type is recognized', () => {
+    expect(ticketList([], { type: null })).toEqual([]);
   });
 
+  test('imported value should match action type string', () => {
+    expect(c.ADD_TICKET).toEqual('testing testing 123');
+  });
 
 });
